9583d82ba9c166ecbe6bd20303d20b84f7330dfa
Seranin <auchoban@gmail.com>
2023-03-18 23:37:57 +0100

    Basic ship movement

--- Pirate game, 2d edition/Assets/Scripts/BasicMovement.cs
+++ Pirate game, 2d edition/Assets/Scripts/BasicMovement.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    public float movespeed = 5f;
-    private Rigidbody2D ship;
+    public float moveSpeed = 5f;
+    public float maxSpeed = 10f;
+    public float rotateForce = 0.1f;
+    private Quaternion rotation;
+    private Rigidbody2D rb;
+    Vector3 MousePos;
+    Vector2 direction = new Vector2(0f,0f);
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (MousePos - transform.position).normalized ;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rotation = Quaternion.AngleAxis( angle - 90, Vector3.forward);
+        transform.rotation = rotation;
+    }
+
+    private void  FixedUpdate()
+    {
+        rb.AddForce(direction * moveSpeed);
+        float anglediff = Vector3.Angle(transform.position, direction);
+        Vector3 cross = Vector3.Cross(transform.up, direction);
+        rb.AddTorque(anglediff * cross.z * rotateForce);
     }
 }
