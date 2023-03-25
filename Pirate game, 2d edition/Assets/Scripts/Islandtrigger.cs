05729d4499ff38bfc651ed8db181cffc9002b936
Seranin <auchoban@gmail.com>
2023-03-25 13:42:01 +0100

    A bti of work
    Added island triggers to chance scnes

---
+++ Pirate game, 2d edition/Assets/Scripts/Islandtrigger.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Islandtrigger : MonoBehaviour
+{
+    public bool closeToAnIsland = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(closeToAnIsland)
+        {
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("It works");
+        closeToAnIsland = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        closeToAnIsland = false;
+    }
+}
